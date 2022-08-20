058fde2fab65350bed873b11392ebd4ebd9a74fa
OmerDokur <111247123+OmerDokur@users.noreply.github.com>
2022-08-20 13:52:28 +0200

    Add files via upload

---
+++ c1e05a2.java
@@ -0,0 +1,11 @@
+package javaexercises;
+
+public class c1e05a2 {
+	public static void main(String[] args) {
+		//Write a program that displays the result of
+	    //(9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)
+		
+		System.out.println("answer=" + ((9.5*4.5)-(2.5*3))/(45.5-3.5));
+
+}
+}
---
+++ c1e06.java
@@ -0,0 +1,11 @@
+package javaexercises;
+
+public class c1e06 {
+	public static void main(String[] args) {
+		//Write a program that displays the result of
+		//1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+		
+		System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9= " + ( 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 ));
+		
+	}
+}
