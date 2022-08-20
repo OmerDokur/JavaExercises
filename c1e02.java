e770a76ee9e33b47f81a7eafcc95b8e30f9e8ea0
OmerDokur <111247123+OmerDokur@users.noreply.github.com>
2022-08-20 14:04:40 +0200

    c1e02

---
+++ c1e02.java
@@ -0,0 +1,14 @@
+package javaexercises;
+
+public class c1e02 {
+	public static void main(String[] args) {
+		//Write a program that displays Welcome to Java five times.
+		System.out.println("" 
+				+ "Welcome To Java\n"
+				+ "Welcome To Java\n"
+				+ "Welcome To Java\n"
+				+ "Welcome To Java\n"
+				+ "Welcome To Java ");
+	}
+
+}
